61e22c5863e34504c0a7a3158a34a05cf00d2a19
Telesita <ana.garcia8523@gmail.com>
2017-10-23 15:12:54 -0500

    version de arreglos 1

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var fila = [1,2,3,4];
-
-fila.push(5);
-
+var objeto = {perrito:'perrito'};
 
 fila.shift();
-document.write(fila)
+document.write(fila);
+fila.push("hola");
+document.write("<br>"+fila);
+fila.pop();
+document.write("<br>"+fila);
+fila.unshift(objeto.perrito);
+document.write("<br>"+fila);
